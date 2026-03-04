53b01359e880eef0c336fa27bd67799d43138c03
Bernadus Dwiki Adicitra <dwiki.adicitra6@gmail.com>
2026-03-04 11:21:20 +0700

    fix: memperbaiki eror pada index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const a = [5, 10, 2, 8]
+console.log(Math.max(...a))
+
+let i = 0;
+let max;
+while (i<a.length){
+    if(i === 0){
+        max = a[i]
+    }else{
+        if(a[i]>max){
+            max = a[i];
+        }
+    }
+    i++
+}
+console.log(max)
